d047c546e25dd33cc88867273c3a16e3d1fdab48
Rhibro <111956763+Rhibro@users.noreply.github.com>
2023-04-17 10:32:59 +0200

    added content to resume

---
+++ src/components/About.js
@@ -0,0 +1,46 @@
+import aboutOne from "../images/aboutImgOne.jpg";
+import aboutTwo from "../images/aboutImgTwo.jpg";
+
+const About = () => {
+
+    return (
+        <div className="aboutBody">
+            <div className="aboutImgBox">
+                <img className="aboutImg" src={aboutOne} alt="profile of Karen"></img>
+                <img className="aboutImg" src={aboutTwo} alt="Karen smiling"></img>
+            </div>
+            <div className="aboutInfoBox">
+                <p className="aboutInfo">
+                Karen is a British/Icelandic actress who received a BA(Hons) in acting from The Kogan Academy of
+                 Dramatic Arts in 2016. Whilst training Karen played roles such as Dottie (‘Killer Joe’) and Kim 
+                 (‘Caravan’). After graduating she first appeared in ‘Extremities’ during The Brighton and Camden 
+                 Fringe Festivals playing the lead, Marjorie.
+                </p>
+                <p className="aboutInfo">
+                She has since been in various short films and fringe theatre productions in London. Roles she has 
+                played include Lady Macbeth (‘Macbeth’), Belle (‘A Christmas Carol’) and Helena (‘A Midsummer’s Night Dream’).
+                </p>
+                <div>
+                    <ul>
+                        <li className="aboutListI">-Height: 5"5</li>
+                        <li className="aboutListI">-Hair Colour: Blonde</li>
+                        <li className="aboutListI">-Eye Colour: Blue</li>
+                        <li className="aboutListI">-Body type: Slim</li>
+                        <li className="aboutListI">-Nationalities: British, Icelandic</li>
+                        <li className="aboutListI">-Appearance: Scandinavian, White</li>
+                        <li className="aboutListI">-Playing Age: 25 to 35</li>
+                    </ul>
+                </div>
+                <p className="aboutInfo">
+                    Location:
+                </p>
+                <p className="aboutInfo">
+                London, UK || Malmö, Sweden || Reykjavík, Iceland
+                </p>
+            </div>
+        </div>
+    )
+
+}
+
+export default About;
---
+++ src/components/Resume.js
@@ -0,0 +1,106 @@
+import resumeImg from "../images/resumeImg.jpg";
+
+const Resume = () => {
+
+return (
+    <div className="resumeBody">
+        <div className="columnR">
+            <img className="resumeimg" src={resumeImg} alt="Karen in a black leather jacket looking serious"></img>
+            <h2 className="resumeTiltles">
+                Special Skills
+            </h2>
+            <ul>
+                <li className="listIr">
+                    Languages: Icelandic*,  Swedish
+                </li>
+                <li className="listIr">
+                    Accents: RP*, Hightened RP, Liverpool, London
+                </li>
+                <li className="listIr">
+                    Sports: Swimming*, Cycling, Pole Dancing, Boxing*, 
+                    Weight Lifting, Aerobics, Running, Hiking, Skiing, 
+                    Jazz Ballet, Yoga (Vinyasa)
+                </li>
+                <li className="listIr">
+                    Singing: Mezzo-Soprano
+                </li>
+                <li className="listIr">
+                    Stage Combat: Rapier & Dagger and Unarmed BASSC certificate
+                </li>
+                <li className="listIr">
+                    Drivers Licence
+                </li>
+            </ul>
+            <h2 className="resumeTiltles">
+                Training 
+            </h2>
+                <p className="resumeItems">
+                    The Kogan Academy of Dramatic Arts | Directing Course | 2016
+                </p>
+                <p className="resumeItems">
+                    The Kogan Academy of Dramatic Arts | BA (Hons Acting) | 2012-2015
+                </p>
+        </div>
+        <div className="columnR">
+        <h2 className="resumeTiltles">
+            Film/TV
+        </h2>
+            <p className="resumeItems">
+                Off the Beaten Path | Djammviskubit (Short film) | 2019 | Stina
+            </p>
+            <p className="resumeItems">
+                Yellow Kite Films | From London with Love (TV Pilot) | 2018 | Anna
+            </p>
+            <p className="resumeItems">
+                Reverie Motion Pictures | External Gazer (short film) | 2017​ | Melissa
+            </p>
+            <p className="resumeItems">
+                Westminster University| Cleaner Conscience (short film) | 2017 | Jane Doe
+            </p>
+            <p className="resumeItems">
+                London Film School | Artificial White (short film) | 2016 | N29
+            </p>
+            <p className="resumeItems">
+                One Man Film | Consequence (short film) | 2016 | Elsa
+            </p>
+        <h2 className="resumeTiltles">
+            Stage
+        </h2>
+            <p className="resumeItems">
+                Misenscene Productions | The Weak Ones | 2019
+                Adele in 'Facing Death' & Miss Y in 'The Stronger'
+            </p>
+            <p className="resumeItems">
+                Splats Entertainment | Romeo & Juliet | 2019 | Juliet & Tybalt
+            </p>
+            <p className="resumeItems">
+                Tenth Muse Productions | Beauty and the Beast | 2018 | Beauty
+            </p>
+            <p className="resumeItems">
+                Misenscene Productions | A Christmas Carol | 2017 | Belle
+            </p>
+            <p className="resumeItems">
+                Immersive Music | Hounds of Love | 2017 | Crow Creature
+            </p>
+            <p className="resumeItems">
+                Splats Entertainment | Macbeth | 2017 | Lady Macbeth & Witch 1
+            </p>
+            <p className="resumeItems">
+                Splats Entertainment | A Midsummer Nights Dream | 2017 | Helena
+            </p>
+            <p className="resumeItems">
+                Sofa Productions | Extremities | 2016 | Marjorie
+            </p>
+        <h2 className="resumeTiltles">
+            Commercials
+        </h2>
+            <p className="resumeItems">
+                Localove | Localove app online | 2017 | Lead Actress
+            </p>
+        </div>
+    </div>
+)
+
+}
+
+export default Resume;
